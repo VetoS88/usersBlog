46362a75d2f13ff64616b5f9e487f2e523d32e7a
VetoS88 <symphonyofbody@gmail.com>
2016-12-05 23:53:49 +0300

    add base template feeds template

--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from .models import Post
 
-# Create your views here.
+def feeds(request):
+    pass
+
+
+def user_blog(request):
+    pass
+
+
+def blogs(request):
+    pass
+
+
+def main(request):
+    posts = Post.objects.all()
+    return render_to_response('blog/feeds.html',
+                              {
+                                  'feeds': posts,
+                              })
+
+

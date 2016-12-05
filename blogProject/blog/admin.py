98f96408fa59b9b6b7a15f13f5c83223bf4e8680
vetos_88 <symphonyofbody@gmail.com>
2016-12-05 17:56:33 +0300

    Start App

--- blogProject/blog/admin.py
+++ blogProject/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post, NewsFeed, Blog
 
-# Register your models here.
+
+
--- blogProject/blog/models.py
+++ blogProject/blog/models.py
@@ -1,3 +1,75 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class Blog(m.Model):
+
+    user = m.ForeignKey(
+        'auth.User',
+        verbose_name='Пользователь',
+    )
+
+    class Meta:
+        verbose_name = "Блог"
+        verbose_name_plural = "Блоги"
+
+
+class Post(m.Model):
+
+    blog = m.ForeignKey(
+        Blog,
+        verbose_name='Блог',
+    )
+
+    title = m.CharField(
+        'Заголовок',
+        max_length=200,
+    )
+    text = m.TextField(
+        'Текст сообщения',
+    )
+    created_date = m.DateTimeField(
+        'Создан',
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-created_date", "user"]
+        get_latest_by = "create_date"
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+
+class NewsFeed(m.Model):
+
+    user = m.ForeignKey(
+        'auth.User',
+        verbose_name='Пользователь',
+    )
+    posts = m.ManyToManyField(
+        Blog,
+        verbose_name='Новости',
+    )
+
+    class Meta:
+        verbose_name = "Новостная лента"
+        verbose_name_plural = "Новостные ленты"
+
+
+class Reviewed(m.Model):
+
+    post = m.ManyToManyField(
+        Post,
+    )
+
+    blog = m.ForeignKey(
+        Blog,
+    )
+
+    isreviewed = m.BooleanField(
+        'Просмотрен',
+        default=False,
+    )
+

9e5b8643e2ef33027cc2c847465455fa1ee250c7
vetos_88 <symphonyofbody@gmail.com>
2016-12-09 11:46:12 +0300

    Refactor to CBV

--- blogProject/blog/urls.py
+++ blogProject/blog/urls.py
@@ -1,15 +1,14 @@
-from django.conf.urls import url, include
 from .views import *
-
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
 
-    url(r'^$', news_feed, name="news_feed"),
-    url(r'^personal_blog/$', personal_blog, name="personal_blog"),
-    url(r'^users_blogs/$', users_blogs, name="users_blogs"),
-    url(r'^add_post/$', add_post, name="add_post"),
-    url(r'^subscribe/(?P<blog_id>\d+)/', subscribe, name="subscribe"),
-    url(r'^post/get/(?P<post_id>\d+)/', get_post, name="get_post"),
+    url(r'^$', UserNewsFeed.as_view(), name="news_feed"),
+    url(r'^personal_blog/$', login_required(PersonalBlog.as_view()), name="personal_blog"),
+    url(r'^users_blogs/$', UsersBlogs.as_view(), name="users_blogs"),
+    url(r'^add_post/$', login_required(AddPost.as_view()), name="add_post"),
+    url(r'^subscribe/(?P<blog_id>\d+)/', Subscribe.as_view(), name="subscribe"),
+    url(r'^post/get/(?P<post_id>\d+)/', GetPost.as_view(), name="get_post"),
 
 
 ]
--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -1,71 +1,87 @@
 from django.conf.urls import url
 from django.contrib import auth
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, render_to_response, redirect
+from django.views import View
+from django.views.generic import RedirectView
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 from blog.forms import PostCreateFoorm
 from .models import Post, NewsFeed, Blog
 
 
-def news_feed(request):
-    user = auth.get_user(request)
-    if not user.is_authenticated:
-        return redirect('/users_blogs/')
-    newsfeed = NewsFeed.objects.get(user=user)
-    userblogs = newsfeed.blogs.all()
-    outposts = []
-    for blog in userblogs:
+class UserNewsFeed(TemplateView):
+    template_name = 'blog/NewsFeed.html'
+
+    def get(self, request, *args, **kwargs):
+        user = auth.get_user(request)
+        if not user.is_authenticated:
+            return redirect('/users_blogs/')
+        newsfeed = NewsFeed.objects.get(user=user)
+        userblogs = newsfeed.blogs.all()
+        outposts = []
+        for blog in userblogs:
+            posts = blog.post_set.all()
+            for post in posts:
+                outposts.append(post)
+        return render(request,
+                      self.template_name,
+                      {
+                          'blogs': userblogs,
+                          'posts': outposts,
+                          'user': user,
+                      })
+
+
+class PersonalBlog(TemplateView):
+    template_name = 'blog/PersonalBlog.html'
+
+    def get(self, request, *args, **kwargs):
+        user = auth.get_user(request)
+        blog = Blog.objects.get(user=user)
         posts = blog.post_set.all()
-        for post in posts:
-            outposts.append(post)
-
-    return render_to_response('blog/NewsFeed.html',
-                              {
-                                  'blogs': userblogs,
-                                  'posts': outposts,
-                                  'user':  user,
-                              })
-
-@login_required
-def personal_blog(request):
-    user = auth.get_user(request)
-    blog = Blog.objects.get(user=user)
-    posts = blog.post_set.all()
-    return render_to_response('blog/PersonalBlog.html',
-                              {
-                                  'blog': blog,
-                                  'posts': posts,
-                                  'user': user,
-                              })
+        return render(request,
+                      self.template_name,
+                      {
+                          'blog': blog,
+                          'posts': posts,
+                          'user': user,
+                      })
 
 
-def users_blogs(request):
-    blogs = Blog.objects.all()
-    user = auth.get_user(request)
-    if not user.is_authenticated:
-        user = None
-    return render_to_response('blog/UsersBlogs.html',
-                              {
-                                  'blogs': blogs,
-                                  'user': user,
-                              })
+class UsersBlogs(TemplateView):
+    template_name = 'blog/UsersBlogs.html'
+
+    def get(self, request, *args, **kwargs):
+        blogs = Blog.objects.all()
+        user = auth.get_user(request)
+        if not user.is_authenticated:
+            user = None
+        return render(request,
+                      self.template_name,
+                      {
+                          'blogs': blogs,
+                          'user': user,
+                      })
 
 
-def get_post(request, post_id):
-    user = auth.get_user(request)
-    post = Post.objects.get(id=post_id)
-    return render_to_response('blog/Post.html',
-                              {
-                                  'post': post,
-                                  'user': user,
-                              })
+class GetPost(TemplateView):
+    template_name = 'blog/Post.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(GetPost, self).get_context_data(**kwargs)
+        post_id = context['post_id']
+        context['post'] = Post.objects.get(id=post_id)
+        return context
 
 
-@login_required
-def add_post(request):
-    user = auth.get_user(request)
-    blog = Blog.objects.get(user=user)
-    if request.method == 'POST':
+class AddPost(TemplateView):
+    template_name = 'blog/AddPost.html'
+    form_class = PostCreateFoorm
+
+    def post(self, request, *args, **kwargs):
+        user = auth.get_user(request)
+        blog = Blog.objects.get(user=user)
         post = request.POST
         createform = PostCreateFoorm(post)
         if createform.is_valid():
@@ -73,26 +89,24 @@ def add_post(request):
             new_post.blog_id = blog.id
             new_post.save()
         return render(request,
-                      'blog/AddPost.html',
+                      self.template_name,
                       {
                           'postform': createform,
                       })
-    else:
-        createform = PostCreateFoorm()
 
-    if request.method == 'GET':
+    def get(self, request, *args, **kwargs):
         return render(request,
-                      'blog/AddPost.html',
-                      {'postform': createform})
-    return render(request,
-                  'blog/AddPost.html',
-                  {'postform': createform})
+                      self.template_name,
+                      {
+                          'postform': self.form_class
+                      })
 
 
-def subscribe(request, blog_id):
-    blog = Blog.objects.get(id=blog_id)
-    user = auth.get_user(request)
-    user_news_feed = NewsFeed.objects.get(user=user)
-    user_news_feed.blogs.add(blog)
-    return redirect('/users_blogs/')
-
+class Subscribe(View):
+    def dispatch(self, request, *args, **kwargs):
+        blog_id = kwargs['blog_id']
+        blog = Blog.objects.get(id=blog_id)
+        user = auth.get_user(request)
+        user_news_feed = NewsFeed.objects.get(user=user)
+        user_news_feed.blogs.add(blog)
+        return redirect('/users_blogs/')

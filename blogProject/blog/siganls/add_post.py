0db72bc86c62feae8ef1fd76ab0b861623239152
vetos_88 <symphonyofbody@gmail.com>
2016-12-28 13:22:46 +0300

    mail delivery

--- blogProject/blog/siganls/add_post.py
+++ blogProject/blog/siganls/add_post.py
@@ -1,10 +1,25 @@
+from django.core import mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from blog.models import Post
+from blog.models import Post, NewsFeed
+from blogProject import settings
+from blogProject.settings import EMAIL_HOST_USER
 
 
 @receiver(post_save, sender=Post)
-def add_post(instance, **kwargs):
-    print("Creates is" + str(kwargs['created']))
-    print("Created post is " + instance.title)
+def add_post(created, instance, **kwargs):
+    blog = instance.blog
+    newsfeeds = NewsFeed.objects.filter(blogs=blog)
+    messages_list = []
+    for newsfeed in newsfeeds:
+        email = newsfeed.user.email
+        if email:
+            subject = "В блоге {} новый пост".format(blog.name)
+            body = "Тема {}. Опубликовано {}. Пользователь {}".format(instance.title,
+                                                                      instance.created_date,
+                                                                      blog.user)
+            message = (subject, body, EMAIL_HOST_USER, [email])
+            messages_list.append(message)
 
+    success = mail.send_mass_mail(messages_list, fail_silently=False)
+    print(success)
